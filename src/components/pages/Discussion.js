0a955ff7a01373458457447fdfdffc142e7f5eed
Ceavin Rufus <16521304@std.stei.itb.ac.id>
2022-08-05 14:40:26 +0700

    activity log

--- src/components/pages/Discussion.js
+++ src/components/pages/Discussion.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import BrowseTopics from "../BrowseTopics";
+import ActivityLog from "../ActivityLog";
 import StudyRooms from "../StudyRooms";
 
 const Discussion = () => {
   return (
-    <div className="bg-[#F58F00] h-screen py-4 px-12">
+    <div className="bg-[#F58F00] h-screen py-8 px-12">
       <div className="grid grid-cols-[1fr_4fr_1fr] gap-8 text-[#fff] font-bold">
         <BrowseTopics />
         <StudyRooms />
-        <div className="flex justify-end">
-          <h1>RECENT ACTIVITIES</h1>
-        </div>
+        <ActivityLog />
       </div>
     </div>
   );
