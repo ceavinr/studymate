e110e0c37e9dc072f666083c91bfd295680476fa
Ceavin Rufus <16521304@std.stei.itb.ac.id>
2022-08-05 14:41:44 +0700

    update topics

--- src/components/BrowseTopics.js
+++ src/components/BrowseTopics.js
@@ -7,39 +7,30 @@ const topics = [
     name: "SPARTA",
     total: 2,
   },
+  {
+    id: 2,
+    name: "Sistem dan Arsitektur Komputer",
+    total: 1,
+  },
 ];
 
 const BrowseTopics = () => {
   return (
-    <div className="flex-grow">
-      <div className="flex mb-16">
+    <div className="flex-grow divide-y-2">
+      <div className="flex mb-2">
         <h1>BROWSE TOPICS</h1>
       </div>
       <div className="space-y-2">
-        <div className="flex flex-col">
-          <div className="flex items-center">
-            <div className="w-full flex-wrap">
-              <Link to={"/discussion"} className="">
-                All
-              </Link>
-            </div>
-            <div className="bg-[#FFCC85] rounded-full w-8 h-8 flex items-center justify-center">
-              2
-            </div>
-          </div>
-        </div>
         {topics.map((topic) => (
-          <div className="flex flex-col">
-            <div className="flex items-center">
-              <div className="w-full flex-wrap">
-                <Link to={"/?topic=" + topic.name} className="">
-                  {topic.name}
-                </Link>
+          <div className="flex flex-col bg-[#fff] hover:bg-[#FFCC85] px-4 py-2 shadow-md rounded-xl mt-4 transition duration-200">
+            <Link to={"/?topic=" + topic.name} className="">
+              <div className="flex items-center">
+                <div className="w-full flex-wrap text-[#000]">{topic.name}</div>
+                <div className="bg-[#F58F00] rounded-full w-8 h-8 flex items-center justify-center">
+                  {topic.total}
+                </div>
               </div>
-              <div className="bg-[#FFCC85] rounded-full w-8 h-8 flex items-center justify-center">
-                {topic.total}
-              </div>
-            </div>
+            </Link>
           </div>
         ))}
       </div>
