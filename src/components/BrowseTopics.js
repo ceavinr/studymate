8b359ac67db84e1085f5bec532e4fa09221f358f
Ceavin Rufus <16521304@std.stei.itb.ac.id>
2022-08-12 20:12:03 +0700

    all topics filter

--- src/components/BrowseTopics.js
+++ src/components/BrowseTopics.js
@@ -5,6 +5,7 @@ import RoomsCard from "./RoomsCard";
 
 const BrowseTopics = ({ changeRoom, rooms }) => {
   const [topics, setTopics] = useState([]);
+  const [active, setActive] = useState("");
 
   useEffect(() => {
     axios
@@ -12,6 +13,11 @@ const BrowseTopics = ({ changeRoom, rooms }) => {
       .then((res) => setTopics(res.data))
       .catch((err) => console.log(err));
   }, []);
+
+  const onChangeRoom = (value) => {
+    changeRoom(value);
+    setActive(value);
+  };
 
   return (
     <div className="flex-grow divide-y-2">
@@ -21,7 +27,7 @@ const BrowseTopics = ({ changeRoom, rooms }) => {
       <div className="space-y-2">
         <div
           className="flex flex-col bg-[#fff] hover:bg-[#FFCC85] px-4 py-2 shadow-md rounded-xl mt-4 transition duration-200 cursor-pointer"
-          onClick={(e) => changeRoom("All")}
+          onClick={(e) => onChangeRoom("All")}
         >
           <div className="flex items-center">
             <div className="w-full flex-wrap text-[#000]">All</div>
@@ -33,7 +39,10 @@ const BrowseTopics = ({ changeRoom, rooms }) => {
         {topics.map((topic) => (
           <div
             className="flex flex-col bg-[#fff] hover:bg-[#FFCC85] px-4 py-2 shadow-md rounded-xl mt-4 transition duration-200 cursor-pointer"
-            onClick={(e) => changeRoom(topic.name)}
+            onClick={(e) => onChangeRoom(topic.name)}
+            style={{
+              backgroundColor: active === topic.name ? "#FFCC85" : "",
+            }}
           >
             <div className="flex items-center">
               <div className="w-full flex-wrap text-[#000]">{topic.name}</div>
