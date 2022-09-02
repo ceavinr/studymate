f8435c123720a68581fa0acd0f2e1e205b02e45b
ceavinr <16521304@std.stei.itb.ac.id>
2022-09-02 23:26:05 +0700

    cors origin

--- src/backend/index.js
+++ src/backend/index.js
@@ -33,7 +33,7 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://studymate.vercel.app/",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
